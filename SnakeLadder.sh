41f9084f95322e13ddad1e6f60f535c9493ee5b7
sheetalc818 <sheetalc818@gmail.com>
2020-01-30 12:57:49 +0530

    Added : Player checking for the Option

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -4,11 +4,31 @@ echo "Snake & Ladder Simulator"
 #Declaring Variables
 position=0
 rollDieNumber=0
+noPlay=0
+snake=1
+ladder=2
 
 function rollDie()
 {
 	rollDieNumber=$((RANDOM%6 + 1))
 	echo $rollDieNumber
 }
-#Calling rollDie Function
+
+function playerAction()
+{
+	actionRoll=$((RANDOM%3))
+	case $actionRoll in
+							$noPlay) echo "No Step"
+										;;
+							$snake)	echo "Snake"
+										position=$((position-$rollDieNumber))
+										;;
+							$ladder) echo "Ladder"
+										position=$((position+$rollDieNumber))
+										;;
+	esac
+}
+
+#Calling Functions
 rollDie
+playerAction $rollDieNumber
