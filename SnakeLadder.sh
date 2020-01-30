0adf13e4c689f9991d790c479bf898e23a1c6cad
sheetalc818 <sheetalc818@gmail.com>
2020-01-30 12:30:23 +0530

    Added : Rolling a die to get a number between 1 to 6

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -3,3 +3,12 @@ echo "Snake & Ladder Simulator"
 
 #Declaring Variables
 position=0
+rollDieNumber=0
+
+function rollDie()
+{
+	rollDieNumber=$((RANDOM%6 + 1))
+	echo $rollDieNumber
+}
+#Calling rollDie Function
+rollDie
